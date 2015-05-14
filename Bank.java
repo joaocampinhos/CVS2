4701bcb0ce111761aba6e605174c76e0f8f042ef
João Campinhos <joao.campinhos@gmail.com>
2015-05-14 10:57:25 +0100

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,7 +1,28 @@
+/*
+VERIFAST example of verified Java progra
+CVS course 14-15
+Integrated Master in Computer Science and Engineering
+@FCT UNL Luis Caires 2015
+*/
 
 /*@
-predicate AccountP(unit a,BankAccount c; unit b) = c != null &*& AccountInv(c,?n,?m) &*& b == unit;
+predicate AccountP(unit a,BankAccount c; int n) = 
+c != null &*& AccountInv(c,n,?m);
 @*/
+
+/*@
+predicate BankInv(Bank bk; int n, int m) = 
+     bk.nelems |-> n &*& 
+     bk.capacity |-> m &*& 
+     m > 0 &*&
+     bk.store |-> ?a &*&
+     a != null &*&
+     a.length == m &*&
+     0 <= n &*& n<=m &*& 
+     array_slice_deep(a, 0, n, AccountP, unit,_,?bs) &*&
+     array_slice(a, n, m,?rest) &*& all_eq(rest, null) == true ;
+@*/
+
 
 class Bank {
 
@@ -9,21 +30,10 @@ BankAccount store[];
 int nelems;
 int capacity;
 
-/*@
-predicate BankInv(int n, int m) = 
-     this.nelems |-> n &*& 
-     this.capacity |-> m &*& 
-     m > 0 &*&
-     this.store |-> ?a &*&
-     a.length == m &*&
-     0 <= n &*& n<=m &*& 
-     array_slice_deep(a, 0, n, AccountP, unit,_, _) &*&
-     array_slice(a, n, m,?rest) &*& all_eq(rest, null) == true ;
-@*/
 
 public Bank(int siz)
 //@ requires siz>0;
-//@ ensures BankInv(0,siz);
+//@ ensures BankInv(this,0,siz);
 {
   nelems = 0;
   capacity = siz;
@@ -31,11 +41,11 @@ public Bank(int siz)
 }
 
 void addnewAccount(int code)
-//@ requires BankInv(?n,?m) &*& n < m &*& code >= 0;
-//@ ensures  BankInv(n+1,m);
+//@ requires BankInv(this,?n,?m) &*& n < m &*& code >= 0;
+//@ ensures  BankInv(this,n+1,m);
 {
 
- 	//@ array_slice_split(store, nelems,nelems+1);
+ 	// @ array_slice_split(store, nelems,nelems+1);
  	BankAccount c = new BankAccount(code);
 	store[nelems] = c;
 	//@ array_slice_deep_close(store, nelems, AccountP, unit);
@@ -43,14 +53,14 @@ void addnewAccount(int code)
 }
 
 int getbalance(int code)
-//@ requires BankInv(?n,?m);
-//@ ensures BankInv(n,m);
+//@ requires BankInv(this,?n,?m);
+//@ ensures BankInv(this,n,m);
 {
   int i = 0;
 
-  //@ open BankInv(n,m);
+  //@ open BankInv(this,n,m);
   while (i < nelems)
-  //@ invariant BankInv(n,m) &*& 0 <= i &*& i <= n;
+  //@ invariant BankInv(this,n,m) &*& 0 <= i &*& i <= n;
   {
      if ( store[i].getcode() == code) {
      	return store[i].getbalance();
@@ -58,6 +68,59 @@ int getbalance(int code)
      i = i + 1;
   }
   return -1;
+}
+
+int removeAccount(int code)
+//@ requires BankInv(this,?n,?m);
+//@ ensures result == 0 ? BankInv(this,n-1,m) : BankInv(this,n,m);
+{
+  int i = 0;
+  //@ open BankInv(this,n,m);
+  while (i < nelems)
+  //@ invariant BankInv(this,n,m) &*& 0 <= i &*& i <= n;
+  {
+     if ( store[i].getcode() == code) {
+     	   if (i<nelems-1) {
+     	     store[i] == store[nelems-1];
+     	   }
+     	   nelems = nelems - 1;
+     	   store[nelems] = null;
+     	 return 0;
+     }
+     i = i + 1;
+  }
+  return -1;
+}
+
+void extendstore()
+//@ requires BankInv(this,?n,?m);
+//@ ensures BankInv(this,n,2*m);
+{
+  int i = 0;
+  BankAccount newstore[] = new BankAccount[capacity*2];
+  //@ open BankInv(this,n,m);
+  while (i < nelems)
+  /*@  invariant
+       this.nelems |-> n &*& 
+       this.capacity |-> m &*&
+       this.store |-> ?a &*&
+       a.length == m &*&
+       0 <= n &*& n<=m &*& 
+       array_slice(a, 0, i,?r1) &*& all_eq(r1, null) == true  &*&
+       array_slice_deep(a, i, n, AccountP, unit,_,?bs) &*&
+       array_slice(a, n, m,?rest) &*& all_eq(rest, null) == true &*&
+       array_slice_deep(newstore, 0, i, AccountP, unit,_,_) &*&
+       array_slice(newstore, i, m*2,?r2) &*& all_eq(r2, null) == true 
+       &*& 0 <= i &*& i <= n;
+     @*/
+  {
+     newstore[i] = store[i];
+     store[i] = null;
+     i++;
+  }
+  capacity = 2*capacity;
+  store = newstore;
+  return;
 }
 
 }
